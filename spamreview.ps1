eddf3940054201a763ccfeba2e5a2657ec005349
astearns64 <astearns64@gmail.com>
2018-06-11 15:17:18 -0700

    Create spamreview.ps1

---
+++ spamreview.ps1
@@ -0,0 +1,13 @@
+$Path = "C:\Program Files (x86)\Parallels\Plesk\Mail Servers\Mail Enable\Logging\SMTP"
+$FileType = "SMTP-Activity-*.log"
+$OutputFile = "C:\temp\spamreview.txt" #This is the location where it will print the results from the command.
+$i = Get-ChildItem $Path -Filter $FileType|
+        Get-Content |
+            Select-String -Pattern 'FROM:<'
+                
+[Regex]::Matches($i, '(?<=\<).+?(?=\>)') | 
+    %{$_.value} |
+        Group-Object -NoElement |
+            Sort-Object -Descending Count|
+                FT -Auto |
+		                Out-File -FilePath $OutputFile
